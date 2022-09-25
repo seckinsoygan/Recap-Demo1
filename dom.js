4059db89434c9807c9d3331ae5981bf357dc0387
seckin <seckin@SECKINSOYGAN>
2022-09-26 01:29:32 +0300

    Dom Manipulation added

---
+++ dom.js
@@ -0,0 +1,20 @@
+//document.getElementById("bio").innerHTML="Seckin Soygan : 1998"
+
+// var intro1=document.getElementById("intro1");
+
+// var mesaj=document.getElementById("mesaj");
+
+// mesaj.innerHTML=intro1.innerHTML;
+
+// var tumlisteler=document.getElementsByTagName("ul"); //Tag name üzerinden erişim sağlar. Bütün ul lere erişir.
+// var sehirler=tumlisteler[0];
+
+// tumElemanlar=sehirler.getElementsByTagName("li");
+
+// for(i=0;i<tumElemanlar.length;i++){
+//     alert(tumElemanlar[i].innerHTML);
+// }
+
+var classElemanları=document.getElementsByClassName("intro1");
+
+
